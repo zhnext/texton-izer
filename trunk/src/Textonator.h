15e84068f6fc47206ee3c5fc8af3430fc76b9fe5
morsela <morsela@7f168336-c49e-11dd-bdf1-993be837f8bb>
2008-12-13 22:12:28 +0000

    Some day, some changes.

--- trunk/src/Textonator.h
+++ trunk/src/Textonator.h
@@ -1,10 +1,15 @@
 #ifndef _H_SEGMENTATOR_H_
 #define _H_SEGMENTATOR_H_
 
+#include <vector>
+
 #include <cv.h>
 #include <highgui.h>
 
 #include "fe/FeatureExtraction.h"
+#include "Texton.h"
+
+using std::vector;
 
 #define FIRST_TEXTON_NUM	5
 #define UNCLUSTERED_DATA	0
@@ -13,37 +18,38 @@
 
 #define EDGE_DATA			255
 
+#define UNDEFINED			-1
+
 class Textonator
 {
 public:
 	Textonator(IplImage * Img, int nClusters, int nMinTextonSize);
 	virtual ~Textonator();
 
-	void Textonize();
+	void	Textonize(vector<Texton*>& textonList);
 
 private:
 	
-	void ColorWindow(int x, int y, int sizeX, int sizeY);
-	void SaveImage(char *filename,IplImage* img);
-	void RecolorPixel(uchar * pData, int y, int x, int step, CvScalar * pColor);
+	void	ColorWindow(int x, int y, int sizeX, int sizeY);
+	void	RecolorPixel(uchar * pData, int y, int x, int step, CvScalar * pColor);
 
-	void Segment();
-	void Cluster(CFeatureExtraction *pFeatureExtractor);
+	void	Segment();
+	void	Cluster(CFeatureExtraction *pFeatureExtractor);
 
-    void colorCluster(int nCluster);
-	void blurImage();
-	void cannyEdgeDetect();
+    void	colorCluster(int nCluster);
+	void	blurImage();
+	void	cannyEdgeDetect();
+	void	extractTextons(int nCluster, vector<Texton*>& textonList);
 
-	void extractTexton(int minX, int maxX, int minY, int maxY, uchar * pImageData, IplImage* pTexton);
+	int		scanForTextons(int nCluster, int * pTextonMap);
+	void	colorTextonMap(uchar *pBorderData,int * pTextonMap,int nCluster);
+	void	assignTextons(int x, int y, uchar * pData, int * pTextonMap, int nClust);
+	void	retrieveTextons(int nTexton, int nCluster, int * pTextonMap, vector<Texton*>& textonList);
+	
+	void	assignRemainingData(int * pTextonMap);
+	void	assignStrayPixels(int ** ppTextonMap, int nSize);
 
-	void extractTextons(int nCluster);
-	int scanForTextons(int nCluster, int * pTextonMap);
-	void colorTextons(int nTexton, int nCluster, int * pTextonMap);
-	void colorTextonMap(uchar *pBorderData,int * pTextonMap,int nCluster);
-	void assignTextons(int x, int y, uchar * pData, int * pTextonMap, int nClust);
-
-	void fixTexton(int nCurDataType, int nValidData1, int nValidData2, int * pTextonMap);
-	void assignRemUntextoned(int * pTextonMap);
+	void	extractTexton(int minX, int maxX, int minY, int maxY, uchar * pImageData, IplImage* pTexton);
 
 private:
 
